94874282e15ae1b164b400f4d12144b85285b29a
Mike Taylor <mike@indexdata.com>
2021-03-08 12:40:53 +0000

    Initial attempt at allowing module types to be selected
    It looks like all the state-handling is correct, but the chart
    dimensions and scaling are all over the places as changes are made.

--- src/settings/Dependencies.js
+++ src/settings/Dependencies.js
@@ -1,16 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { HorizontalBar } from 'react-chartjs-2';
 import { stripesConnect } from '@folio/stripes/core';
-import { LoadingPane, Pane } from '@folio/stripes/components';
+import { Checkbox, LoadingPane, Pane } from '@folio/stripes/components';
 
-
-// By inspection, module type can be determined from ID. Four types:
-//      /folio_(.*)-([0-9].)*/    UI module $1, version $2
-//      /mod-(.*)-([0-9].)*/      Back-end module $1, version $2
-//      /edge-(.*)-([0-9].)*/     Edge module $1, version $2
-//      /(.*)-([0-9].)*/          Other module $1, version $2 -- e.g. okapi
 
 function chartModules(intl, records) {
   const names = records.map(r => r.name);
@@ -66,11 +60,34 @@ function chartModules(intl, records) {
   );
 }
 
+
 const Dependencies = ({ resources }) => {
-  const { modules } = resources;
   const intl = useIntl();
 
+  const [includeUI, setIncludeUI] = useState(true);
+  const [includeBackend, setIncludeBackend] = useState(true);
+  const [includeEdge, setIncludeEdge] = useState(true);
+  const [includeOther, setIncludeOther] = useState(true);
+
+  const { modules } = resources;
   if (!modules.hasLoaded) return <LoadingPane />;
+
+  // By inspection, module type can be determined from ID. Four types:
+  //      /folio_(.*)-([0-9].)*/    UI module $1, version $2
+  //      /mod-(.*)-([0-9].)*/      Back-end module $1, version $2
+  //      /edge-(.*)-([0-9].)*/     Edge module $1, version $2
+  //      /(.*)-([0-9].)*/          Other module $1, version $2 -- e.g. okapi
+  const active = [];
+  modules.records.forEach(module => {
+    const { id } = module;
+    if ((!includeUI && !includeBackend && !includeEdge && !includeOther) ||
+        (includeUI && id.startsWith('folio_')) ||
+        (includeBackend && id.startsWith('mod-')) ||
+        (includeEdge && id.startsWith('edge-')) ||
+        (includeOther && (!id.startsWith('folio_') && !id.startsWith('mod-') && !id.startsWith('edge-')))) {
+      active.push(module);
+    }
+  });
 
   const lcn = (x) => x.name.toLowerCase();
   modules.records.sort((a, b) => (lcn(a) < lcn(b) ? -1 : lcn(a) > lcn(b) ? 1 : 0));
@@ -80,7 +97,31 @@ const Dependencies = ({ resources }) => {
       defaultWidth="fill"
       paneTitle={<FormattedMessage id="ui-developer.dependencies" />}
     >
-      {chartModules(intl, modules.records)}
+      <Checkbox
+        checked={includeUI}
+        data-test-checkbox-include-ui-modules
+        label={<FormattedMessage id="ui-developer.dependencies.ui-modules" />}
+        onChange={e => setIncludeUI(e.target.checked)}
+      />
+      <Checkbox
+        checked={includeBackend}
+        data-test-checkbox-include-backend-modules
+        label={<FormattedMessage id="ui-developer.dependencies.backend-modules" />}
+        onChange={e => setIncludeBackend(e.target.checked)}
+      />
+      <Checkbox
+        checked={includeEdge}
+        data-test-checkbox-include-edge-modules
+        label={<FormattedMessage id="ui-developer.dependencies.edge-modules" />}
+        onChange={e => setIncludeEdge(e.target.checked)}
+      />
+      <Checkbox
+        checked={includeOther}
+        data-test-checkbox-include-other-modules
+        label={<FormattedMessage id="ui-developer.dependencies.other-modules" />}
+        onChange={e => setIncludeOther(e.target.checked)}
+      />
+      {chartModules(intl, active)}
       <br />
       <hr />
       <p>
