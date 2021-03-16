66e6530eac8fd65e8723c529b7feaa7db10a3401
Mike Taylor <mike@indexdata.com>
2021-03-16 19:16:22 +0000

    Add human-readable permission name to "Current permissions" list.
    Fixes UID-73.

--- src/settings/ShowPermissions.js
+++ src/settings/ShowPermissions.js
@@ -2,12 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
+import { stripesConnect } from '@folio/stripes/core';
 import { Pane, List } from '@folio/stripes/components';
 
 const ShowPermissions = (props) => {
-  const { stripes } = props;
+  const { stripes, resources } = props;
   const currentPerms = stripes.user ? stripes.user.perms : {};
-  const perms = Object.keys(currentPerms).sort();
+
+  const id2name = {};
+  resources.permissions.records.forEach(perm => {
+    id2name[perm.permissionName] = perm.displayName;
+  });
+
+  const perms = Object.keys(currentPerms).sort().map(key => (
+    <><b>{key}</b> ({id2name[key]})</>
+  ));
 
   return (
     <Pane
@@ -22,12 +31,26 @@ const ShowPermissions = (props) => {
   );
 };
 
+ShowPermissions.manifest = Object.freeze({
+  permissions: {
+    type: 'okapi',
+    path: 'perms/permissions',
+    params: { length: '9999' },
+    records: 'permissions',
+  },
+});
+
 ShowPermissions.propTypes = {
   stripes: PropTypes.shape({
     user: PropTypes.shape({
       perms: PropTypes.object.isRequired,
     }).isRequired,
   }).isRequired,
+  resources: PropTypes.shape({
+    permissions: PropTypes.shape({
+      records: PropTypes.arrayOf(PropTypes.object),
+    }),
+  }).isRequired,
 };
 
-export default ShowPermissions;
+export default stripesConnect(ShowPermissions);
