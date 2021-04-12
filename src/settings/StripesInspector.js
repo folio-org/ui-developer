d3e9c3d1c95d738093518e7d1e6d86c0f8e02c8e
Zak Burke <zburke@cornell.edu>
2021-04-12 17:48:50 -0400

    UID-77 show stripes object (#233)
    Deserialize and display the `stripes` object which may be usesful for
    devs attempting to debug things.
    
    Refs UID-77

---
+++ src/settings/StripesInspector.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Pane } from '@folio/stripes/components';
+import ObjectInspector from 'react-inspector';
+
+const StripesInspector = ({ stripes }) => {
+  return (
+    <Pane
+      defaultWidth="fill"
+      paneTitle={<FormattedMessage id="ui-developer.stripesInspector" />}
+    >
+      <ObjectInspector
+        data={stripes}
+        expandLevel={1}
+        sortObjectKeys
+      />
+    </Pane>
+  );
+};
+
+StripesInspector.propTypes = {
+  stripes: PropTypes.object.isRequired,
+};
+
+export default StripesInspector;
