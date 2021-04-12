5daaa4b27f6839f19150ea1f7dc3ab48d34d37bf
Zak Burke <zburke@cornell.edu>
2021-04-12 13:46:42 -0400

    UID-75 handle AST-ified values
    When a translation file has been translated to an AST, its values will
    no longer be simple strings. Naively parse these AST objects so the
    values back to strings.
    
    Refs UID-75

--- src/settings/Translations.js
+++ src/settings/Translations.js
@@ -3,9 +3,43 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Pane } from '@folio/stripes/components';
 
+function renderArray(a) {
+  return a.map(t => {
+    let str;
+    switch (t.type) {
+      case 1: // placeholders for strings, e.g. {foo}
+      case 2: // placeholders with types, e.g. {foo, number}
+        str = `{${t.value}}`;
+        break;
+      case 8: // html elements, e.g. <strong>foo</strong>
+        str = `<${t.value}>${renderArray(t.children)}</${t.value}>`;
+        break;
+      case 0: // simple values without placeholders, e.g. foo
+      default:
+        str = t.value;
+        break;
+    }
+    return str;
+  }).join('');
+}
+
+/**
+ * Raw translation values, i.e. those from .json files, are simple strings.
+ * If the file has been transformed into an AST, the value will be an array;
+ * such values are naively un-AST-ified in a manner that seems to work so far.
+ *
+ * @see https://formatjs.io/docs/guides/advanced-usage#pre-compiling-messages
+ *
+ * @param {*} t
+ * @returns
+ */
 function renderTranslation(t) {
   if (typeof t === 'string') {
     return <b>{t}</b>;
+  }
+
+  if (Array.isArray(t)) {
+    return <b>{(t.length === 1) ? t[0].value : renderArray(t)}</b>;
   }
 
   return `[${typeof t}]`;
