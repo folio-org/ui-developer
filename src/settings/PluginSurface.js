1f4c4f4ba85a8d67eab86b74a8e64678ad150868
Mike Taylor <mike@indexdata.com>
2021-04-27 11:27:53 +0100

    Minor improvements to plugin surface, including i18n (#243)

--- src/settings/PluginSurface.js
+++ src/settings/PluginSurface.js
@@ -4,18 +4,23 @@ import { Pluggable } from '@folio/stripes/core';
 import { Pane } from '@folio/stripes/components';
 
 const PluginSurface = () => {
-  const [pluginType, setPluginType] = useState('eusage-reports');
+  const [pluginType, setPluginType] = useState('');
 
   return (
     <Pane
       defaultWidth="fill"
       paneTitle={<FormattedMessage id="ui-developer.plugin-surface" />}
     >
-      Plugin type:
+      <FormattedMessage id="ui-developer.plugin-surface.type" />
       {' '}
       <input type="text" value={pluginType} onChange={(e) => setPluginType(e.target.value)} />
-      <h3>{`${pluginType}`}</h3>
-      <Pluggable type={pluginType} id="pluggable-surface" />
+      <p>
+        <FormattedMessage id="ui-developer.plugin-surface.forExample" values={{ pluginType: 'ui-agreements-extension' }} />
+      </p>
+      <hr />
+      <Pluggable type={pluginType} id="pluggable-surface">
+        <FormattedMessage id="ui-developer.plugin-surface.noPlugin" values={{ pluginType }} />
+      </Pluggable>
     </Pane>
   );
 };
