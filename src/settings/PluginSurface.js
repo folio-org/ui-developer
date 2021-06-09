63e03bf294738f19df1b58010fdc052a45faedc5
Mike Taylor <mike@indexdata.com>
2021-06-09 13:04:32 +0100

    Pluggable-surface page now supports optional data object (#255)
    When provided, this must be a valid JSON object. When entered into the
    provided textarea, the background turns pink when the JSON is invalid,
    and no data object is passed to the plugin; when the JSON is valid,
    the background turns white again and the data object is passed.
    
    Fixes UID-86.

--- src/settings/PluginSurface.js
+++ src/settings/PluginSurface.js
@@ -1,24 +1,77 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Pluggable } from '@folio/stripes/core';
-import { Pane } from '@folio/stripes/components';
+import { Pane, Row, Col } from '@folio/stripes/components';
 
 const PluginSurface = () => {
   const [pluginType, setPluginType] = useState('');
+  const [pluginData, setPluginData] = useState('');
+
+  let dataObject;
+  let dataOK = false;
+  try {
+    dataObject = JSON.parse(pluginData);
+    dataOK = true;
+  } catch (e) {
+    // Nothing to do
+  }
 
   return (
     <Pane
       defaultWidth="fill"
       paneTitle={<FormattedMessage id="ui-developer.plugin-surface" />}
     >
-      <FormattedMessage id="ui-developer.plugin-surface.type" />
-      {' '}
-      <input type="text" value={pluginType} onChange={(e) => setPluginType(e.target.value)} />
-      <p>
-        <FormattedMessage id="ui-developer.plugin-surface.forExample" values={{ pluginType: 'ui-agreements-extension' }} />
-      </p>
-      <hr />
-      <Pluggable type={pluginType} id="pluggable-surface">
+      <Row>
+        <Col xs={4}>
+          <FormattedMessage id="ui-developer.plugin-surface.type" />
+        </Col>
+        <Col xs={8}>
+          <input type="text" value={pluginType} onChange={(e) => setPluginType(e.target.value)} />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={4} />
+        <Col xs={8}>
+          <p>
+            <FormattedMessage
+              id="ui-developer.plugin-surface.exampleType"
+              values={{
+                pluginType: 'ui-agreements-extension',
+                code: chunks => <code>{chunks}</code>,
+              }}
+            />
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={4}>
+          <FormattedMessage id="ui-developer.plugin-surface.data" />
+        </Col>
+        <Col xs={8}>
+          <textarea
+            value={pluginData}
+            style={{ backgroundColor: (dataOK || pluginData === '') ? 'white' : '#fcc' }}
+            onChange={(e) => setPluginData(e.target.value)}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={4} />
+        <Col xs={8}>
+          <p>
+            <FormattedMessage
+              id="ui-developer.plugin-surface.exampleData"
+              values={{
+                data: '{"op":"match-names"}',
+                code: chunks => <code>{chunks}</code>,
+              }}
+            />
+          </p>
+        </Col>
+      </Row>
+
+      <hr style={{ marginTop: '2em' }} />
+      <Pluggable type={pluginType} id="pluggable-surface" data={dataObject}>
         <FormattedMessage id="ui-developer.plugin-surface.noPlugin" values={{ pluginType }} />
       </Pluggable>
     </Pane>
