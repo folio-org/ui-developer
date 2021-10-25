4f6cb553a3e2fd2381c99c483d4f48e597090e81
Mike Taylor <mike@miketaylor.org.uk>
2021-10-25 18:12:07 +0100

    Expands subpermissions for the first time

--- src/settings/PermissionsInspector.js
+++ src/settings/PermissionsInspector.js
@@ -42,25 +42,17 @@ function SinglePermission({ permName, name2perm }) {
         {perm.displayName && (
           <>{' '}(<i>{perm.displayName}</i>)</>
         )}
-        {expanded &&
-          <span>
-            {perm.description &&
-              <div style={{ margin: '0.5em 1em' }}>{perm.description}</div>
-            }
-            {perm.subPermissions.length > 0 &&
-              <ul style={{ listStyleType: 'none' }}>
-                {perm.subPermissions.sort(permNameCmp).map(subPerm => {
-                  return (
-                    <li key={subPerm}>
-                      {subPerm}
-                    </li>
-                  );
-                })}
-              </ul>
-            }
-          </span>
-        }
       </span>
+      {expanded &&
+        <span>
+          {perm.description &&
+            <div style={{ margin: '0.5em 1.5em' }}>{perm.description}</div>
+          }
+          {perm.subPermissions.length > 0 &&
+            <PermissionsList permNames={perm.subPermissions.sort(permNameCmp)} name2perm={name2perm} />
+          }
+        </span>
+      }
     </span>
   );
 }
