592a84c8afa1404ce41b28e0e7bc7262dfeb4f7c
Mike Taylor <mike@miketaylor.org.uk>
2021-11-25 11:53:55 +0000

    Move KeyValue display out into its own component

--- src/settings/OkapiConsole/Parameters.js
+++ src/settings/OkapiConsole/Parameters.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useStripes } from '@folio/stripes/core';
-import css from './OkapiConsole.css';
+import KeyValueList from './KeyValueList';
 
 
 function Parameters() {
   const stripes = useStripes();
   const { okapi } = stripes;
 
-  return (
-    <table className={css.keyValueTable}>
-      <tbody>
-        <tr>
-          <td><FormattedMessage id="ui-developer.okapiConsole.parameters.url" /></td>
-          <td><code>{okapi.url}</code></td>
-        </tr>
-        <tr>
-          <td><FormattedMessage id="ui-developer.okapiConsole.parameters.tenant" /></td>
-          <td><code>{okapi.tenant}</code></td>
-        </tr>
-        <tr>
-          <td><FormattedMessage id="ui-developer.okapiConsole.parameters.user" /></td>
-          <td>
-            <code>{okapi.currentUser.username}</code>
-            {' '}
-            ({okapi.currentUser.firstName}
-            {' '}
-            {okapi.currentUser.lastName})
-          </td>
-        </tr>
-        <tr>
-          <td><FormattedMessage id="ui-developer.okapiConsole.parameters.locale" /></td>
-          <td><code>{okapi.locale}</code></td>
-        </tr>
-      </tbody>
-    </table>
-  );
+  return <KeyValueList
+    dataList={[
+      [
+        <FormattedMessage id="ui-developer.okapiConsole.parameters.url" />,
+        <code>{okapi.url}</code>
+      ],
+      [
+        <FormattedMessage id="ui-developer.okapiConsole.parameters.tenant" />,
+        <code>{okapi.tenant}</code>
+      ],
+      [
+        <FormattedMessage id="ui-developer.okapiConsole.parameters.user" />,
+        <>
+          <code>{okapi.currentUser.username}</code>
+          {' '}
+          ({okapi.currentUser.firstName}
+          {' '}
+          {okapi.currentUser.lastName})
+        </>
+      ],
+      [
+        <FormattedMessage id="ui-developer.okapiConsole.parameters.locale" />,
+        <code>{okapi.locale}</code>
+      ],
+    ]}
+  />;
 }
 
 
