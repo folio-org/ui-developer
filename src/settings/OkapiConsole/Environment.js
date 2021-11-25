20bacae6395fd71319b8970e8e3ce0b58bf1410b
Mike Taylor <mike@miketaylor.org.uk>
2021-11-25 15:37:07 +0000

    Change variable names

--- src/settings/OkapiConsole/Environment.js
+++ src/settings/OkapiConsole/Environment.js
@@ -5,14 +5,14 @@ import KeyValueList from './KeyValueList';
 
 
 function Environment() {
-  const [env, setEnv] = useState();
+  const [data, setData] = useState();
   const [error, setError] = useState();
   const okapiKy = useOkapiKy();
 
   useEffect(() => {
     okapiKy('_/env').then(async res => {
       const text = await res.text();
-      setEnv(text);
+      setData(text);
     }).catch(async e => {
       setError({ summary: e.toString(), detail: await e.response.text() });
     });
@@ -34,12 +34,12 @@ function Environment() {
     );
   }
 
-  if (!env) return <Loading />;
-  const data = JSON.parse(env);
+  if (!data) return <Loading />;
+  const parsed = JSON.parse(data);
 
   // XXX In future we could add facilities for adding, editing and removing entries
   // But for now, listing is sufficient
-  return <KeyValueList dataList={data.map(e => [e.name, e.value])} />;
+  return <KeyValueList dataList={parsed.map(e => [e.name, e.value])} />;
 }
 
 
