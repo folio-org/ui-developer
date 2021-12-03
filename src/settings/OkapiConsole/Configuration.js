c2bf0a190b9b21ca50361062002762693303d3b4
Mike Taylor <mike@indexdata.com>
2021-12-03 12:30:03 +0000

    Okapi version number now appears only in Configuration tab (#300)

--- src/settings/OkapiConsole/Configuration.js
+++ src/settings/OkapiConsole/Configuration.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { useStripes } from '@folio/stripes/core';
+import { useStripes, useOkapiKy } from '@folio/stripes/core';
 import KeyValueList from './KeyValueList';
 
 
 function Configuration() {
+  const [version, setVersion] = useState();
+  const okapiKy = useOkapiKy();
   const stripes = useStripes();
   const { okapi } = stripes;
 
+  useEffect(() => {
+    okapiKy('_/version').then(async res => {
+      const text = await res.text();
+      setVersion(text);
+    });
+  });
+
   return <KeyValueList
     dataList={[
+      [
+        <FormattedMessage id="ui-developer.okapiConsole.configuration.version" />,
+        <code>{version}</code>
+      ],
       [
         <FormattedMessage id="ui-developer.okapiConsole.configuration.url" />,
         <code>{okapi.url}</code>
--- src/settings/OkapiConsole/OkapiConsole.js
+++ src/settings/OkapiConsole/OkapiConsole.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { useOkapiKy } from '@folio/stripes/core';
 import { Pane, ButtonGroup, Button } from '@folio/stripes/components';
 import Configuration from './Configuration';
 import Environment from './Environment';
@@ -17,24 +16,14 @@ const pages = [
 
 
 function OkapiConsole() {
-  const [version, setVersion] = useState();
   const [tab, setTab] = useState('configuration');
-  const okapiKy = useOkapiKy();
   const Component = pages.find(p => p.tab === tab).component;
-
-  useEffect(() => {
-    okapiKy('_/version').then(async res => {
-      const text = await res.text();
-      setVersion(text);
-    });
-  });
 
   return (
     <Pane
       defaultWidth="fill"
       paneTitle={<FormattedMessage id="ui-developer.okapiConsole" />}
     >
-      <h3>Okapi version {version}</h3>
       <ButtonGroup>
         {
           pages.map(page => (
