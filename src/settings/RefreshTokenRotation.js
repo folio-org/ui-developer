35152a00b21fe7c6f09ac2c0311439068edde852
Zak Burke <zburke@ebsco.com>
2024-01-31 12:56:08 -0500

    UID-138 purge service worker cruft; clean up RTR display (#405)
    ui-developer was not sync'ed with stripes-core after the initial
    service-woker based RTR implementation was removed (in Poppy) and
    replaced (in Quesnelia).
    
    Refs UID-138

--- src/settings/RefreshTokenRotation.js
+++ src/settings/RefreshTokenRotation.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import localforage from 'localforage';
 
 import {
-  registerServiceWorker,
-  unregisterServiceWorker,
-  useStripes,
+  getTokenExpiry,
+  setTokenExpiry,
 } from '@folio/stripes/core';
 
 import {
@@ -17,76 +15,46 @@ import {
 } from '@folio/stripes/components';
 
 /**
- * manipulate AT/RT expiration dates in storage in order to test the RTR service worker.
+ * manipulate AT/RT expiration dates in storage in order to test RTR.
  * @returns
  */
 const RefreshTokenRotation = () => {
-  let tokenExpiration = {};
-
   const [isLoading, setIsLoading] = useState(true);
-  const stripes = useStripes();
+  const [tokenExpiration, setTokenExpiration] = useState({});
 
-  localforage.getItem('okapiSess')
-    .then((data) => {
-      tokenExpiration = {
-        atExpires: data.tokenExpiration?.accessTokenExpiration ? new Date(data.tokenExpiration.accessTokenExpiration).getTime() : -1,
-        rtExpires: data.tokenExpiration?.refreshTokenExpiration ? new Date(data.tokenExpiration.refreshTokenExpiration).getTime() : Date.now() + (10 * 60 * 1000),
-      };
-      setIsLoading(false);
-    });
+  useEffect(() => {
+    getTokenExpiry()
+      .then(te => {
+        setTokenExpiration(te ?? { atExpires: -1, rtExpires: -1 });
+        setIsLoading(false);
+      });
+  }, []);
 
-  const dispatchTokenExpiration = () => {
-    stripes.logger.log('rtr', `atExpires ${tokenExpiration.atExpires}`);
-    stripes.logger.log('rtr', `rtExpires ${tokenExpiration.rtExpires}`);
+  /**
+   * invalidateAT
+   * return a promise to expire the AT in local storage
+   */
+  const invalidateAT = () => {
+    return getTokenExpiry()
+      .then(te => {
+        const expiration = { ...te };
+        expiration.atExpires = -1;
 
-    navigator.serviceWorker.ready
-      .then((reg) => {
-        const sw = reg.active;
-        if (sw) {
-          const message = { source: '@folio/stripes-core', type: 'TOKEN_EXPIRATION', tokenExpiration };
-          stripes.logger.log('rtr', '@folio/developer sending', message);
-          sw.postMessage(message);
-        } else {
-          console.warn('could not dispatch message; no active registration'); // eslint-disable-line no-console
-        }
+        return setTokenExpiry(expiration);
       });
   };
 
   /**
-   * invalidateAT
-   * expire the AT and notify the SW
-   */
-  const invalidateAT = () => {
-    tokenExpiration.atExpires = -1;
-    dispatchTokenExpiration();
-  };
-
-  /**
    * invalidateRT
-   * expire the AT and the RT and notify the SW
+   * return a promise to expire the AT and RT in local storage
    */
   const invalidateRT = () => {
-    tokenExpiration.atExpires = -1;
-    tokenExpiration.rtExpires = -1;
+    const expiration = {
+      atExpires: -1,
+      rtExpires: -1,
+    };
 
-    dispatchTokenExpiration();
-  };
-
-  /**
-   * registerSW
-   * Call the service-worker registration function.
-   */
-  const registerSW = () => {
-    registerServiceWorker(stripes.okapi, stripes.logger.categories, stripes.logger);
-  };
-
-  /**
-   * unregisterSW
-   * Call the service-worker remove-registration function. This has the same effect
-   * as using the browser's developer-tools to disable an active service worker.
-   */
-  const unregisterSW = () => {
-    unregisterServiceWorker();
+    return setTokenExpiry(expiration);
   };
 
   if (!isLoading) {
@@ -96,16 +64,19 @@ const RefreshTokenRotation = () => {
         renderHeader={renderProps => <PaneHeader {...renderProps} paneTitle={<FormattedMessage id="ui-developer.rtr" />} />}
       >
         <ul>
-          <li>service-worker logs RTR events in the category <code>rtr-sw</code></li>
           <li>stripes logs RTR events in the category <code>rtr</code></li>
         </ul>
+        {!isLoading && (
+          <dl>
+            <dt>AT Expiry</dt>
+            <dd>{new Date(tokenExpiration.atExpires).toISOString()}</dd>
+            <dt>RT Expiry</dt>
+            <dd>{new Date(tokenExpiration.rtExpires).toISOString()}</dd>
+          </dl>
+        )}
         <div>
           <Button onClick={invalidateAT}><FormattedMessage id="ui-developer.rtr.invalidateAT" /></Button>
           <Button onClick={invalidateRT}><FormattedMessage id="ui-developer.rtr.invalidateRT" /></Button>
-        </div>
-        <div>
-          <Button onClick={registerSW}><FormattedMessage id="ui-developer.rtr.registerServiceWorker" /></Button>
-          <Button onClick={unregisterSW}><FormattedMessage id="ui-developer.rtr.unregisterServiceWorker" /></Button>
         </div>
       </Pane>
     );
