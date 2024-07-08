d25c637bfd6d5fd6594617b7c3eb2bd17900c8eb
Ryan Berger <rberger@ebsco.com>
2024-07-08 15:40:09 -0400

    UID-154 New utility in developer settings for looking up capabilities/capabilitySets by permission name (#423)
    * Add Capability search in devtools
    
    * Formatting fixes
    
    * PR fixes

---
+++ src/settings/ShowCapabilities.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { stripesConnect, useOkapiKy, useStripes } from '@folio/stripes/core';
+import { Button, Pane, Row, Col, SearchField } from '@folio/stripes/components';
+
+const ShowCapabilities = () => {
+  const ky = useOkapiKy();
+  const stripes = useStripes();
+  const [capabilitiesResults, setCapabilitiesResults] = useState({});
+  const [capabilitySetsResults, setCapabilitySetsResults] = useState({});
+  const [query, setQuery] = useState('');
+
+  const searchParams = {
+    limit: stripes.config.maxUnpagedResourceCount,
+    query: `name=*${query}*`,
+  };
+
+  const submit = async () => {
+    const capabilitiesResponse = await ky.get('capabilities', { searchParams }).json();
+    setCapabilitiesResults(capabilitiesResponse);
+
+    const capabilitySetsResponse = await ky.get('capability-sets', { searchParams }).json();
+    setCapabilitySetsResults(capabilitySetsResponse);
+  };
+
+  const displayList = (resultList, listType) => {
+    return resultList[listType]?.map((value) => (
+      <ul>
+        <li>{value.permission}</li>
+        <ul>
+          <li><strong>type:</strong> {value.type}</li>
+          <li><strong>applicationId:</strong> {value.applicationId}</li>
+          <li><strong>resource:</strong> {value.resource}</li>
+          <li><strong>action:</strong> {value.action}</li>
+          <li><strong>permissionName:</strong> {value.name}</li>
+        </ul>
+      </ul>
+    ));
+  };
+
+  return (
+    <Pane
+      defaultWidth="fill"
+      paneTitle={<FormattedMessage id="ui-developer.capabilities" />}
+    >
+      <Row>
+        <Col xs={12}>
+          <h3><FormattedMessage id="ui-developer.capabilitiesSubtitle" /></h3>
+          <SearchField name="query" id="query" value={query} onInput={e => setQuery(e.target.value)} />
+          <Button onClick={submit}><FormattedMessage id="ui-developer.search" /></Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          { capabilitiesResults.capabilities?.length > 0 && <h3><FormattedMessage id="ui-developer.capabilities" /></h3> }
+          { displayList(capabilitiesResults, 'capabilities') }
+          { capabilitySetsResults.capabilitySets?.length > 0 && <h3><FormattedMessage id="ui-developer.capabilitySets" /></h3> }
+          { displayList(capabilitySetsResults, 'capabilitySets') }
+        </Col>
+      </Row>
+    </Pane>
+  );
+};
+
+export default stripesConnect(ShowCapabilities);
