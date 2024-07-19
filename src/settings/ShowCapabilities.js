cb84571fbf76f65cc718ffff927e3b440e7fe205
Ryan Berger <rberger@ebsco.com>
2024-07-19 10:37:53 -0400

    Add support for searching by permission display name

--- src/settings/ShowCapabilities.js
+++ src/settings/ShowCapabilities.js
@@ -2,26 +2,64 @@ import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import { stripesConnect, useOkapiKy, useStripes } from '@folio/stripes/core';
-import { Button, Pane, Row, Col, SearchField } from '@folio/stripes/components';
+import { Button, Pane, Row, Col, SearchField, Select } from '@folio/stripes/components';
 
 const ShowCapabilities = () => {
+  const SEARCH_BY_TYPES = {
+    PERMISSION_DISPLAY_NAME: 'permissionDisplayName',
+    PERMISSION_NAME: 'permissionName'
+  };
+
+  const searchByOptions = [{ label: SEARCH_BY_TYPES.PERMISSION_DISPLAY_NAME, value: SEARCH_BY_TYPES.PERMISSION_DISPLAY_NAME },
+    { label: SEARCH_BY_TYPES.PERMISSION_NAME, value: SEARCH_BY_TYPES.PERMISSION_NAME }];
+
   const ky = useOkapiKy();
   const stripes = useStripes();
   const [capabilitiesResults, setCapabilitiesResults] = useState({});
   const [capabilitySetsResults, setCapabilitySetsResults] = useState({});
   const [query, setQuery] = useState('');
+  const [searchBy, setSearchBy] = useState(SEARCH_BY_TYPES.PERMISSION_DISPLAY_NAME);
 
   const searchParams = {
     limit: stripes.config.maxUnpagedResourceCount,
-    query: `permission=*${query}*`,
+    query: '',
+  };
+
+  const handleSearchByChange = (e) => {
+    setSearchBy(e.target.value);
   };
 
   const submit = async () => {
+    if (searchBy === SEARCH_BY_TYPES.PERMISSION_DISPLAY_NAME) {
+      const searchIds = searchForPermissionDisplayName(query);
+      searchParams.query = `permission=${searchIds.join(' OR permission=')}`;
+    } else {
+      searchParams.query = `permission=*${query}*`;
+    }
+
     const capabilitiesResponse = await ky.get('capabilities', { searchParams }).json();
     setCapabilitiesResults(capabilitiesResponse);
 
     const capabilitySetsResponse = await ky.get('capability-sets', { searchParams }).json();
     setCapabilitySetsResults(capabilitySetsResponse);
+  };
+
+  const searchForPermissionDisplayName = (displayNameQuery) => {
+    let searchIds = [];
+
+    if (stripes.discovery?.permissionDisplayNames) {
+      for (const [key, value] of Object.entries(stripes.discovery.permissionDisplayNames)) {
+        if (value?.toUpperCase().trim().includes(displayNameQuery?.toUpperCase().trim())) {
+          searchIds.push(key);
+        }
+      }
+    }
+    
+    return searchIds;
+  };
+
+  const lookUpPermissionDisplayNameById = (permissionName) => {
+    return stripes.discovery?.permissionDisplayNames?.[permissionName];
   };
 
   const displayList = (resultList, listType) => {
@@ -34,6 +72,7 @@ const ShowCapabilities = () => {
           <li><strong>resource:</strong> {value.resource}</li>
           <li><strong>action:</strong> {value.action}</li>
           <li><strong>permissionName:</strong> {value.permission}</li>
+          <li><strong>permissionDisplayName:</strong> {lookUpPermissionDisplayNameById(value.permission)}</li>
         </ul>
       </ul>
     ));
@@ -45,11 +84,20 @@ const ShowCapabilities = () => {
       paneTitle={<FormattedMessage id="ui-developer.capabilities" />}
     >
       <Row>
-        <Col xs={12}>
-          <h3><FormattedMessage id="ui-developer.capabilitiesSubtitle" /></h3>
-          <SearchField name="query" id="query" value={query} onInput={e => setQuery(e.target.value)} />
-          <Button onClick={submit}><FormattedMessage id="ui-developer.search" /></Button>
-        </Col>
+        <h3><FormattedMessage id="ui-developer.capabilitiesSubtitle" /></h3>
+      </Row>
+      <Row>
+        <SearchField name="query" id="query" value={query} onInput={e => setQuery(e.target.value)} />
+        &nbsp;&nbsp;
+        <Button onClick={submit}><FormattedMessage id="ui-developer.search" /></Button>
+      </Row>
+      <Row>
+        <Select
+          name="searchBy"
+          label={<FormattedMessage id="ui-developer.okapiConfigurationEntries.filterByModule" />}
+          dataOptions={searchByOptions}
+          onChange={handleSearchByChange}
+        />
       </Row>
       <Row>
         <Col xs={12}>
